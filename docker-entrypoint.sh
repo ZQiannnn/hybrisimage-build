bc1730917c36f261da69e3a27347026249c20925
ZQiannnn <604922962@qq.com>
2017-12-25 11:31:45 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
-set -e
-cd $PLATFORM_HOME
-ant clean all production
-
-cd ~ && docker build -t $IMAGE_TAG .
-docker login -u $USERNAME -p $PASSWORD  $REGISTRY
-docker push $IMAGE_TAG
+#!/bin/sh
+echo
+/usr/local/bin/dockerd-entrypoint.sh $DOCKER_OPTS --insecure-registry=$REGISTRY &
+exec $@
